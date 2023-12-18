ebe2750fc24039601a830493c3d7e2f8263b68bf
Samuel Eliab Granados Figuera <samuelgranados2801@gmail.com>
2023-12-18 20:38:13 -0300

    conclusão

--- alura_books-projeto_inicial/app/metodoFilter.js
+++ alura_books-projeto_inicial/app/metodoFilter.js
@@ -4,6 +4,26 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livros => livros.quantidade > 0) : livros.filter(livros => livros.categoria == categoria)
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
+    if (categoria == 'disponivel') {
+        const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados)
+        exibirValorTotalDosLivrosDisponivelNaTela(valorTotal)
+    }
+}
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livros => livros.categoria == categoria)
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livros => livros.quantidade > 0)
+}
+
+function exibirValorTotalDosLivrosDisponivelNaTela(valorTotal) {
+    elementoComValorTotalDeLivrosDisponivel.innerHTML = `
+    <div class="livros__disponiveis">
+      <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+    </div>
+    `
 }
